f08d8dd71408377a23f621b9b6d736780216936e
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-03-19 00:36:01 +0100

    build: Proyecto maven
    Se ha creado el proyecto maven

---
+++ JAVA/pilotoautomatico/src/main/java/gpl/pizarro/pilotoautomatico/App.java
@@ -0,0 +1,13 @@
+package gpl.pizarro.pilotoautomatico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
